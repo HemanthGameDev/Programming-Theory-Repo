9356e6de3c48bd6d42e198a182dd6d7757e1c6d5
Gunti Hemanth Kumar <gunti.hemanth.kumar.dev@gmail.com>
2026-02-27 22:03:24 +0530

    title_menu
    done implementing ui
    also added a field to enter name which can be shown in the other scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private string playerName;
+    public string PlayerName
+    {
+        get { return playerName; } set { playerName = value; }
+    }
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
